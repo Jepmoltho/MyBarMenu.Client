0fc57b4c8de9619440fa16396f540dba0081be6d
Jeppe Mølgaard Thomsen <jeppe.m.thomsen@twoday.com>
2025-07-23 15:34:40 +0200

    authenticting pages first - then endpoints. Still need to streamline the proces of putting bearer token in the header

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -33,46 +33,5 @@ public class UserService : IUserService
         {
             return new List<UserDTO>();
         }
-        //var users = await _httpClient.GetFromJsonAsync<List<UserDTO>>("/users");
-
-        //return users;
     }
-
-    //public async Task<IEnumerable<UserDTO>> GetUsers()
-    //{
-    //    //var token = await _localStorageService.GetItemAsync<string>("authToken");
-
-    //    if (string.IsNullOrEmpty(token))
-    //    {
-    //        return new List<UserDTO>();
-    //    }
-
-    //    //Im authorizing twice both in frontend and backend. Firstly on page level using AuthStateService and in every api call using [Authorize] and sending the token like this
-    //    var request = new HttpRequestMessage(HttpMethod.Get, "users");
-    //    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-    //    var response = await _httpClient.SendAsync(request);
-
-    //    //var response = await _httpClient.GetAsync("users");
-
-    //    if (response.IsSuccessStatusCode)
-    //    {
-    //        var users = await response.Content.ReadFromJsonAsync<List<UserDTO>>();
-
-    //        return users ?? new List<UserDTO>();
-    //    }
-
-    //    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-    //    {
-    //        throw new UnauthorizedAccessException();
-    //        //return new List<UserDTO>(); //ENDS IN HERE.
-    //    }
-
-    //    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-    //    {
-    //        return new List<UserDTO>();
-    //    }
-
-    //    return new List<UserDTO>();
-    //    //throw new Exception($"Failed to fetch users. Status code: {response.StatusCode}");
-    //}
 }
